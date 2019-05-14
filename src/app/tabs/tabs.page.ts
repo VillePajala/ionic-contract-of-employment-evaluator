7b7364c7de566f63b92480118996d5a74212d3ea
VillePajala <valoraami@gmail.com>
2019-05-14 11:21:28 +0300

    Update tabs.page.ts

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
+import { Platform } from '@ionic/angular';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-tabs',
@@ -7,14 +9,17 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor () {
+  constructor (private platform : Platform) {
 
   }
 
   closeApp = () : void => {
     navigator['app'].exitApp();
+    //this.platform.exitApp();
   }
 
 }
+
+
 
 
